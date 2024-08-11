7f63966f1e67507883687db472c253a40f478ce3
sunnybunnyy <kradha6113@gmail.com>
2024-08-11 19:09:59 +0200

    Complete exercise 3 - reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    for (let i = string.length; i > 0; i--) {
+        const firstLetter = string[0];
+        const toBeReveresed = string.slice(1, i);
+        const reversed = string.slice(i);
+        string = toBeReveresed + firstLetter + reversed;
+    }
+    return string;
 };
 
 // Do not edit below this line
