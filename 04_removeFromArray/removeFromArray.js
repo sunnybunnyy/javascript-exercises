3c571141310f39c54b71d3a9f80d847f4fed2c8e
sunnybunnyy <kradha6113@gmail.com>
2024-08-11 21:09:31 +0200

    Complete exercise 4 - removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+    let newArray = [];
+    arrayLoop: for(let i = 0; i < array.length; i++) {
+        const element = array[i];
+        for(const elementToRemove of elementsToRemove) {
+            if(element === elementToRemove) {
+                continue arrayLoop;
+            }
+        }
+        newArray.push(element);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
