940d536b3428ab153c597c834960a57b74430fe6
sunnybunnyy <kradha6113@gmail.com>
2024-08-11 21:19:21 +0200

    Complete exercise 5 - sumALL

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(numStart, numEnd) {
+    let sum = 0;
+    if (typeof(numStart) !== "number" || typeof(numEnd) !== "number" || Math.floor(numStart) !== numStart || Math.floor(numEnd) !== numEnd || numStart < 0 || numEnd < 0) {
+        return 'ERROR';
+    }
+    if (numEnd < numStart) {
+        const temp = numStart;
+        numStart = numEnd;
+        numEnd = temp;
+    }
+    for (;numStart <= numEnd; numStart++) {
+        sum += numStart;
+    }
+    return sum;
 };
 
 // Do not edit below this line
