76ca31e1f20e264c214e3001e7166bba12ec29c1
sunnybunnyy <kradha6113@gmail.com>
2024-08-16 20:17:12 +0200

    Complete exercise 10 - fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) { 
+        return "OOPS";
+    }
 
+    let num1 = -1;
+    let num2 = 1;
+    let sum = num1 + num2;
+
+    for (let i = 1; i <= num; i++) {
+        num1 = num2;
+        num2 = sum;
+        sum = num1 + num2;
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+//  -1 1 0 1 1 2 3 5 8
