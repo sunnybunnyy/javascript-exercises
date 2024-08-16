e591ddd4668fe3983f5776c71c35597ab6f29c21
sunnybunnyy <kradha6113@gmail.com>
2024-08-16 20:03:43 +0200

    Complete exercise 9 - palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    let cleanPhrase = phrase.replaceAll(',', '')
+                            .replaceAll('.', '')
+                            .replaceAll(`'`, '')
+                            .replaceAll(' ', '')
+                            .replaceAll('!', '')
+                            .toLowerCase();
+    for (let i = 0; i < cleanPhrase.length / 2; i++) {
+        if (cleanPhrase.at(i) !== cleanPhrase.at(-i - 1)) {
+            return false;
+        }
+    }
 
+    return true;
 };
 
 // Do not edit below this line
