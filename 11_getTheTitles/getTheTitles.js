671d09a918083ea6dcdcccd49993a4d512e7135c
sunnybunnyy <kradha6113@gmail.com>
2024-08-16 20:25:40 +0200

    Complete exercise 11 - getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = books.map(element => element.title);
+    return titles;
 };
 
 // Do not edit below this line
